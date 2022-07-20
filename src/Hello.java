84df7551ad3aee47532c81dfae8975026d0efe7c
Vincent <vincenthabon3@gmail.com>
2022-07-20 21:31:38 +0800

    At this time I add another variable which is the string and integer

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, Vincent Jose C Habon");
+        int firstDeclarationOfVariable = 22 ; /*this line is a declaration statement*/
+        String Name = "Vincent Jose C. Habon";
+        System.out.println("my name is "+Name + "My age at this year 2022 is "+firstDeclarationOfVariable);
+
     }
 }
