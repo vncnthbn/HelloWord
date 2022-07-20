39da8120ab7d9d27af3ad476a178d1d47a8e95d2
Vincent <vincenthabon3@gmail.com>
2022-07-20 22:10:03 +0800

    add another functions like creating second class then getting another data or using another data from other classes the code was define at the line 19

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,27 @@
-public class Hello {
+public class Hello  {
+
+
+
+    protected static class Hello2{
+
+        int a = 20;
+        int b = 30;
+
+
+    }
     public static void main(String[] args){
         System.out.println("Hello, Vincent Jose C Habon");
         int firstDeclarationOfVariable = 22 ; /*this line is a declaration statement*/
+        int sencondVariable = 23;
+        int addTwoIntegers = sencondVariable + firstDeclarationOfVariable;
+        String concatinating = String.valueOf(addTwoIntegers + firstDeclarationOfVariable);
+        int PEMDAS = 10 + 29 - (2* 87);
+        Hello2  classB = new Hello2();/*use data from another class declared the class then define its name*/
+
         String Name = "Vincent Jose C. Habon";
         System.out.println("my name is "+Name + "My age at this year 2022 is "+firstDeclarationOfVariable);
-
+        System.out.println(addTwoIntegers + "Value of addTwointegers and first declaration" +concatinating);
+        System.out.println(PEMDAS);
+        System.out.println(classB.a);
     }
 }
